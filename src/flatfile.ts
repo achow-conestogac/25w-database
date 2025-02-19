9db75623403c48e79b8a1328b3d16f48e1b3dcf0
Andy Chow <achow@conestogac.on.ca>
2025-02-19 16:50:58 -0500

    creating a constructor that will create a flatfileDirectory directory

--- src/flatfile.ts
+++ src/flatfile.ts
@@ -3,12 +3,24 @@
 // 	capacity: number,
 // }
 // 
+import fs from "fs";
+import path from "path";
+
 interface ParkingLot {
   name: string,
   capacity: number,
 }
 
-export default class Flatfile {}
+export default class Flatfile {
+  constructor() {
+  const projectRoot = path.resolve(__dirname, '../..');
+  
+  // Define the directory path to check
+  const dirPath = path.join(projectRoot, 'flatfileDirectory');
+
+  fs.mkdirSync(dirPath)
+  }
+}
 
 export function create(
   data: ParkingLot,
